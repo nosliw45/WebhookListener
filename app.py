6dc342bdd1dbf765888b8839384e762ea3e35bf0
nosliw45 <97943466+nosliw45@users.noreply.github.com>
2022-02-07 21:40:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,23 @@
-#Ref: https://flask.palletsprojects.com/en/2.0.x/quickstart/
-#Ref: https://hackersandslackers.com/flask-routes/
-#Import the Flask class and related functions used in this app
-from flask import Flask, request, redirect, url_for, render_template, Response
+from flask import Flask, render_template, jsonify, request, session
 
-#Create an instance of the Flask class and call it as the name 'app'
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
-def respond():
-  print(request.json);
-  return Response(status=200)
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template('index.html', **locals())
 
 
-#app.route("/") is call to the root URL and will execute the codes in the 'home' view function
-@app.route("/")
-def home():
-  #Redirect(url_for ......) is to redirect the user to the path specified in this case /printMessage and pass the variable 'message' over
-  return redirect(url_for('printMessage', message='Hello its me!!'))
 
-#Call to the /printMessage URL. It also accepts a second argument which is a list of accepted HTTP methods in this case GET and POST
-#and will execute the codes in the 'printMessage' view function which will accept an argument 'message'
-@app.route("/printMessage/<message>", methods=['GET','POST'])
-def printMessage(message):
-  #Execute if the HTML returns back a POST method
-  if request.method == 'POST':
-    #return render_template will call an HTML page to the user in this case 'post.html' which by default located in the 'templates' directory
-    return render_template('post.html',message='This page is called through a POST method')
-  #'index.html' will be called first in this view function, only when there is a POST, then 'post.html' will be called
-  return render_template('index.html',message=message)
+@app.route('/chatbot', methods=["GET", "POST"])
+def chatbotResponse():
 
-  
+    if request.method == 'POST':
+        the_question = request.form['question']
+        #[response, tempName] = processor.chatbot_response(the_question, sessionName)
+
+    return jsonify({"response": "this is a hard-coded response from Wilson" })
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8888', debug=True)
