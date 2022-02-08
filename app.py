2bb8682bc47ea0163bc8dc401a8f6050a4f1c700
nosliw45 <97943466+nosliw45@users.noreply.github.com>
2022-02-08 12:46:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,19 @@ from flask import Flask, render_template, jsonify, request, session, logging, Re
 
 app = Flask(__name__)
 
-#app.config['SECRET_KEY'] = 'enter-a-very-secretive-key-3479373'
+app.config['SECRET_KEY'] = 'enter-a-very-secretive-key-3479373'
 
-#quizResults = None
+quizResults = None
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html', **locals())
 
 
-
 @app.route('/webhook', methods=["GET", "POST"])
 def chatbotResponse():
     global quizResults
         
-    '''
     if request.method == 'GET':
         return "You sent a GET request"
     elif request.method == 'POST':
@@ -31,16 +29,17 @@ def chatbotResponse():
            
     # return jsonify({"response": "this is a hard-coded response from Wilson" })
     # return Response(status=200)
-   return request.json
+    return request.json
 
-'''
+
+
 @app.route('/quizresults', methods=["GET", "POST"])
 def getQuizResults():
     global quizResults
 
     print(quizResults)
     return quizResults
-'''
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8888', debug=True)
