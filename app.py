a386a4de74881bf4a8cdb08209c544fc3a15edad
nosliw45 <97943466+nosliw45@users.noreply.github.com>
2022-02-08 10:50:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,19 +14,22 @@ def chatbotResponse():
     # app.logger.setLevel(logging.DEBUG)
     app.logger.info("[chatbotResponse]")
     
+    '''
     if request.method == 'GET':
         return "You sent a GET request"
     elif request.method == 'POST':
         return "You sent a POST request"
     else:
         return "You sent an unknown request type"
+    '''
     
     #if request.method == 'POST':
         #the_question = request.form['question']
         #[response, tempName] = processor.chatbot_response(the_question, sessionName)
 
     # return jsonify({"response": "this is a hard-coded response from Wilson" })
-    return Response(status=200)
+    # return Response(status=200)
+    return request.json
 
 
 
